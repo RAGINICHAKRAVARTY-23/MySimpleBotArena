1d8a5c099f785ec8e7b16e668bb8e38e2d153b92
RaginiChakravarty <raginichakravarty2011@gmail.com>
2025-10-29 13:09:53 +0530

    Updated MakeHeart.py

---
+++ MakeHeart.py
@@ -0,0 +1,26 @@
+import turtle
+
+
+screen = turtle.Screen()
+screen.bgcolor("pink")
+
+t = turtle.Turtle()
+t.speed(10)
+t.pensize(5)
+t.fillcolor('red')
+t.pencolor('black')
+
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90, 200)
+t.setheading(60)
+t.circle(-90, 200)
+t.forward(180)
+t.end_fill()
+
+t.hideturtle()
+turtle.done()
+
+
+
